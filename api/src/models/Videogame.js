b1b227b88d15f2f9acf729062d4f3253e8b305a8
GSantostefano <gabrielsantostefano@gmail.com>
2023-09-13 17:17:04 -0300

    003
    modelo videogame

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,41 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description:{
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    plataforms: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    image: {
+      type: DataTypes.STRING,
+      defaultValue: "UnKnown",
+      allowNull: true,
+    },
+    released: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    website: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: "Unknown"
+    },
+    rating: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+      defaultValue: "Unknown"
+    }
   });
 };
