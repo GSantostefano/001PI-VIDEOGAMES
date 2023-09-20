5188a0c72d24f0d8cd955cba25d1f7cb11cf96c7
GSantostefano <gabrielsantostefano@gmail.com>
2023-09-20 12:06:57 -0300

    landing
    landing

--- client/src/views/landing/landing.jsx
+++ client/src/views/landing/landing.jsx
@@ -9,14 +9,9 @@ function Landing() {
       <div className={style.Container}>
       <h1>¡Henry Videogames!</h1>
       <h4>El sitio indicado para conocer la información</h4>
-      <h4>Para próxima aventura gaming</h4>
-
-      <button className={style.landingPageButton}>Click aqui para descubrirla</button>
-
-      
+      <h4>De tu próxima aventura gaming</h4>
+      <button className={style.landingPageButton}>Click aqui para descubrirla</button>      
       </div>
-
-      <h6>proyecto impulsado por RAWG.io</h6>
 
      </div>
     
